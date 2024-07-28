13978b3d39736f9095e132812ff59a273b7b2ea5
drtaguewsu <r.tague@westernsydney.edu.au>
2024-07-28 03:47:09 +0000

    Remove snippets

--- code-snippet-2.js
+++
@@ -1,5 +0,0 @@
-async function fetchData(apiUrl) {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    return data;
-}
--- code-snippet-4.js
+++
@@ -1,4 +0,0 @@
-function calculateDiscount(price, discount) {
-    const finalPrice = price - price * discount;
-    return finalPrice;
-}
